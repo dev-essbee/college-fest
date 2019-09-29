de802842358185f0ec2399f78cc7558fdf299bec
Shubham Bhandari <dev.sb18@gmail.com>
2019-09-29 10:54:51 +0530

    animations run npm install and restart server'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
+import {NgwWowService} from 'ngx-wow';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private wowSubscription: Subscription;
+
+  constructor(private router: Router, private wowService: NgwWowService) {
+
+  }
+
   title = 'sabrang';
+
+
+  ngOnInit(): void {
+    console.log('app-component-on-init');
+
+    this.wowService.init();
+  }
+
+  ngOnDestroy(): void {
+    this.wowSubscription.unsubscribe();
+  }
+
+
 }
