6b7fbc53f136af456fa0608cfbab2bfa2d5ddb17
Shubham Bhandari <dev.sb18@gmail.com>
2019-09-30 18:41:21 +0530

    fixed names

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -9,18 +9,18 @@ export class TeamComponent implements OnInit {
   emailSb = 'mailto:dev.sb18@gmail.com?cc=kumarrohitbindal@gmail.com&subject=Regarding sabrang website&body=Hi';
   emailRb = 'mailto:kumarrohitbindal@gmail.com?cc=dev.sb18@gmail.com&subject=Regarding sabrang website&body=Hi';
 
-  faculty = [{name: 'Dr. Loknath Mishra', contact: '9999558286'}, {
+  faculty = [{name: 'Dr. Lokanath Mishra', contact: '9999558286'}, {
     name: 'Mr. Sami Ur Rehman',
     contact: '9982226747'
   }];
-  students = [{name: 'Vishnu Agrawal', contact: '8085194808'}, {
+  students = [{name: 'Vishnu Agarwal', contact: '8085194808'}, {
     name: 'Somesh Sharma',
     contact: '9509703636'
   }, {name: 'Payal Sharma', contact: '8112288381'}, {
     name: 'Ashish Kumar',
     contact: '9602777992'
   }, {name: 'Karan Sharma<', contact: '9958433968'}, {
-    name: 'Sonashi Jhawar',
+    name: 'Sonakshi Jhawar',
     contact: '9571547052'
   }, {name: 'Saransh Gupta', contact: '8535008333'}];
   web = [{
