541c9b72b42f5277f354de7587c81c5e10d1348f
Shubham Bhandari <dev.sb18@gmail.com>
2019-09-30 14:14:57 +0530

    images compressed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const gulp = require('gulp');
+const imageMin = require('gulp-imagemin');
+
+function imgCompress() {
+  return gulp
+    .src("./src/assets/img/**/**/*")
+    .pipe(imageMin())
+    .pipe(gulp.dest("./src/assets/minimg"));
 }
 
-exports.default = defaultTask
+gulp.task("imgCompress", imgCompress);
+
+gulp.task("watch", () => {
+  gulp.watch("./src/assets/img/*", imgCompress);
+});
+
+gulp.task("default", gulp.series('imgCompress', 'watch'));
