0fe65102f9b7d59b9cfed88f11f47caf7c1ae827
Shubham Bhandari <dev.sb18@gmail.com>
2019-09-30 18:50:32 +0530

    firebase fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD5L9wWWUBwQbxgK5dBiaAYPWByhNWyPns',
+    authDomain: 'sabrang-web.firebaseapp.com',
+    databaseURL: 'https://sabrang-web.firebaseio.com',
+    projectId: 'sabrang-web',
+    storageBucket: '',
+    messagingSenderId: '132210288017',
+    appId: '1:132210288017:web:3006522c24294457e12630'
+  }
 };
